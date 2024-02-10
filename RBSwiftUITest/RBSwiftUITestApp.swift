9ebcded262431bf140f8583ba774a74c6a2c76c1
Nikolay <1inchunikhin@gmail.com>
2024-02-11 02:03:52 +0300

    Initial Commit

---
+++ RBSwiftUITest/RBSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBSwiftUITestApp.swift
+//  RBSwiftUITest
+//
+//  Created by Николай Чунихин on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RBSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
